cf20cf3cb9d8d9ff2d4aeecbefa7fad9c137a3ea
PhuQuy <hoangtuthatinh2114@gmail.com>
2015-05-29 12:00:31 +0700

    change .

--- app/src/main/java/android/phuquy/dev/cashmedia/Utils/Config.java
+++ app/src/main/java/android/phuquy/dev/cashmedia/Utils/Config.java
@@ -3,61 +3,68 @@ package android.phuquy.dev.cashmedia.Utils;
 import android.phuquy.dev.cashmedia.R;
 
 public class Config {
-	public static final String YOUR_EMAIL = "cashapp@gmail.com";
-	public static final String LINK_TO_SHARE = "http://google.com.vn";
-	
-	//do not change this
-	public static final String TAG_CONTROLLER = "controller";
-	public static final String TAG_SUCCESS = "success";
-	public static final String TAG_MESSAGE = "message";
+    public static final String YOUR_EMAIL = "cashapp@gmail.com";
+    public static final String LINK_TO_SHARE = "http://google.com.vn";
 
-	public static final int NET_ICON[] = {R.drawable.ic_supersonic,R.drawable.ic_matomy,R.drawable.ic_radium,R.drawable.ic_launcher};
-	public static final String NET_NAME[] ={"SuperSonic","Matomy","RadiumOne","Super Reward"};
-	public static final String[] POINTS = { "1000","2000", "5000", "10000", "25000",
-			"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"
-			,"1000","2000", "5000", "10000", "25000"};
-	public static final int REWARD_ICON [] = {
-			R.drawable.ic_paypal,R.drawable.ic_paypal,R.drawable.ic_paypal,R.drawable.ic_paypal,R.drawable.ic_paypal,
-			R.drawable.ic_itunes,R.drawable.ic_itunes,R.drawable.ic_itunes,R.drawable.ic_itunes,R.drawable.ic_itunes,
-			R.drawable.ic_amazon_version,R.drawable.ic_amazon_version,R.drawable.ic_amazon_version,R.drawable.ic_amazon_version,R.drawable.ic_amazon_version,
-			R.drawable.ic_ebay,R.drawable.ic_ebay,R.drawable.ic_ebay,R.drawable.ic_ebay ,
-			R.drawable.ic_googleplay,R.drawable.ic_googleplay,R.drawable.ic_googleplay,
-			R.drawable.ic_gamestop,R.drawable.ic_gamestop,
-			R.drawable.ic_rizty,
-			R.drawable.ic_bestbuy,
-			R.drawable.ic_trion,
-			R.drawable.ic_xbox,
-			R.drawable.ic_applebee,
-			R.drawable.ic_gap,
-			R.drawable.ic_sears,
-			R.drawable.ic_starbucks,
-	};
-	public static final String[] PP_REWARDS = {
-			"1$ Paypal USD Cash","$2 Paypal USD Cash","$5 Paypal USD Cash", "$10 Paypal USD Cash", "$25 Paypal USD Cash",
-			"1$ iTunes USD Cash","$2 iTunes Canada Gift USD Cash","$5 iTunes USD Cash", "$10 iTunes Canada Gift USD Cash", "$25 iTunes USD Cash",
-			"$1 Amazon.com Gift Cash","$2 Amazon.com Gift Cash","$5 Amazon.com Gift Cash", "$10 Amazon.com Gift Cash", "$25 Amazon.com Gift Cash",
-			"$2 eBays Gift Cash","$5 eBays Gift Cash", "$10 eBays Gift Cash", "$25 eBays Gift Cash",
-			"$5 Google Play Gift Cash", "$10 Google Play Gift Cash", "$25 Google Play Gift Cash",
-			"$5 GameStop Gift Cash", "$10 GameStop Gift Cash",
-			"$5 Rixty Gift Cash",
-			"$5 Trion Gift Cash",
-			"$5 Best Buy Gift Cash",
-			"$5 XBox Digital Gift Cash",
-			"$5 Gap Gift Cash",
-			"$5 Applebee's Gift Cash",
-			"$10 Sears Gift Cash",
-			"$10 Starbucks Gift Cash",
+    //do not change this
+    public static final String TAG_CONTROLLER = "controller";
+    public static final String TAG_SUCCESS = "success";
+    public static final String TAG_MESSAGE = "message";
 
-	};
-	
-	public static final String PREFERENCES_NAME = "cashapp";
-	public static final String PREFERENCES_KEY = "whatthefuck";
-	public static final String KEY_LOGGED = "logged";
+    public static final int NET_ICON[] = {R.drawable.ic_supersonic, R.drawable.ic_matomy, R.drawable.ic_radium, R.drawable.ic_launcher};
+    public static final String NET_NAME[] = {"SuperSonic", "Matomy", "RadiumOne", "Super Reward"};
+    public static final String[] POINTS = {
+            "1000", "2000", "5000", "10000", "25000",
+            "1000", "2000", "5000", "10000", "25000"
+            , "1000", "2000", "5000", "10000", "25000"
+            , "2000", "5000", "10000", "25000"
+            , "5000", "10000", "25000"
+            , "5000", "10000"
+            , "5000"
+            , "5000"
+            , "5000"
+            , "5000"
+            , "5000"
+            , "5000"
+            , "10000"
+            , "10000"
+
+    };
+    public static final int REWARD_ICON[] = {
+            R.drawable.ic_paypal, R.drawable.ic_paypal, R.drawable.ic_paypal, R.drawable.ic_paypal, R.drawable.ic_paypal,
+            R.drawable.ic_itunes, R.drawable.ic_itunes, R.drawable.ic_itunes, R.drawable.ic_itunes, R.drawable.ic_itunes,
+            R.drawable.ic_amazon_version, R.drawable.ic_amazon_version, R.drawable.ic_amazon_version, R.drawable.ic_amazon_version, R.drawable.ic_amazon_version,
+            R.drawable.ic_ebay, R.drawable.ic_ebay, R.drawable.ic_ebay, R.drawable.ic_ebay,
+            R.drawable.ic_googleplay, R.drawable.ic_googleplay, R.drawable.ic_googleplay,
+            R.drawable.ic_gamestop, R.drawable.ic_gamestop,
+            R.drawable.ic_rizty,
+            R.drawable.ic_bestbuy,
+            R.drawable.ic_trion,
+            R.drawable.ic_xbox,
+            R.drawable.ic_applebee,
+            R.drawable.ic_gap,
+            R.drawable.ic_sears,
+            R.drawable.ic_starbucks,
+    };
+    public static final String[] PP_REWARDS = {
+            "1$ Paypal Gift Cash", "$2 Paypal Gift Cash", "$5 Paypal Gift Cash", "$10 Paypal Gift Cash", "$25 Paypal Gift Cash",
+            "1$ iTunes Gift Cash", "$2 iTunes Canada Gift  Cash", "$5 iTunes Gift Cash", "$10 iTunes Canada Gift  Cash", "$25 iTunes Gift Cash",
+            "$1 Amazon.com Gift Cash", "$2 Amazon.com Gift Cash", "$5 Amazon.com Gift Cash", "$10 Amazon.com Gift Cash", "$25 Amazon.com Gift Cash",
+            "$2 eBays Gift Cash", "$5 eBays Gift Cash", "$10 eBays Gift Cash", "$25 eBays Gift Cash",
+            "$5 Google Play Gift Cash", "$10 Google Play Gift Cash", "$25 Google Play Gift Cash",
+            "$5 GameStop Gift Cash", "$10 GameStop Gift Cash",
+            "$5 Rixty Gift Cash",
+            "$5 Trion Gift Cash",
+            "$5 Best Buy Gift Cash",
+            "$5 XBox Digital Gift Cash",
+            "$5 Gap Gift Cash",
+            "$5 Applebee's Gift Cash",
+            "$10 Sears Gift Cash",
+            "$10 Starbucks Gift Cash",
+
+    };
+
+    public static final String PREFERENCES_NAME = "cashapp";
+    public static final String PREFERENCES_KEY = "whatthefuck";
+    public static final String KEY_LOGGED = "logged";
 }
--- app/src/main/java/android/phuquy/dev/cashmedia/fragment/RewardsFragment.java
+++ app/src/main/java/android/phuquy/dev/cashmedia/fragment/RewardsFragment.java
@@ -12,45 +12,88 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class RewardsFragment extends Fragment {
-	private ListView listview;
-	private FragmentViewClickListener listener;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_rewards, container,
-				false);
-		listview = (ListView) view.findViewById(R.id.lvPaypal);
+    private static final String TAG = RewardsFragment.class.getSimpleName();
+    private ListView listview;
+    private TextView mTxtTitle;
+    private FragmentViewClickListener listener;
 
-		RewardAdapter adapter = new RewardAdapter(getActivity(),
-				R.layout.rewards_items,Config.REWARD_ICON, Config.PP_REWARDS);
-		listview.setAdapter(adapter);
-		listview.setOnItemClickListener(new OnItemClickListener() {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_rewards, container,
+                false);
+        listview = (ListView) view.findViewById(R.id.lvPaypal);
+        mTxtTitle = (TextView) view.findViewById(R.id.title_reward);
 
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				listener.onListRewardsClick(arg2);
-			}
-		});
 
-		return view;
-	}
-	
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-		listener = (FragmentViewClickListener) activity;
-	}
+        RewardAdapter adapter = new RewardAdapter(getActivity(),
+                R.layout.rewards_items, Config.REWARD_ICON, Config.PP_REWARDS);
+        listview.setAdapter(adapter);
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		Log.d("qqq", "Reward");
-	}
+        listview.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                Log.d(TAG, "firstVisibleItem" + firstVisibleItem);
+                if (firstVisibleItem < 4) {
+                    mTxtTitle.setText("PayPal Cash");
+                } else if (firstVisibleItem > 4 && firstVisibleItem < 9) {
+                    mTxtTitle.setText("iTunes Cash");
+
+                } else if (firstVisibleItem > 9 && firstVisibleItem < 14) {
+                    mTxtTitle.setText("Amazon Cash");
+
+                } else if (firstVisibleItem > 14 && firstVisibleItem < 18) {
+                    mTxtTitle.setText("eBays Cash");
+
+                } else if (firstVisibleItem > 19 && firstVisibleItem < 21) {
+                    mTxtTitle.setText("Google Play Cash");
+
+                } else if (firstVisibleItem > 21 && firstVisibleItem < 23) {
+                    mTxtTitle.setText("Game Stop Cash");
+
+                } else if (firstVisibleItem > 23) {
+                    mTxtTitle.setText("Other");
+                }
+//				Log.d(TAG,""+totalItemCount);
+            }
+        });
+
+        listview.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+
+                listener.onListRewardsClick(position);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        listener = (FragmentViewClickListener) activity;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("qqq", "Reward");
+    }
+
+
 }
